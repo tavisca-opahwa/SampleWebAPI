b83e4101932a4f8a7afc44d62b0f2fce6fbc2200
Pahwa <opahwa@internal.tavisca.com>
2019-08-14 13:05:43 +0530

    Sample web API added

---
+++ API/Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Bye";
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            if (id == "Hi")
+                return "Hello";
+            else if (id == "Hello")
+                return "Hi";
+            else
+                return "Say Hello or Hi First";
+        }
+    }
+}
