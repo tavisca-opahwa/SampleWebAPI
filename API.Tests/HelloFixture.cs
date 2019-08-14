daad4e2816e28ff2679ce9a34bb42b9abd482a20
Pahwa <opahwa@internal.tavisca.com>
2019-08-14 13:39:50 +0530

    Updated Test File

--- API.Tests/HelloFixture.cs
+++ API.Tests/HelloFixture.cs
@@ -1,14 +1,34 @@
+using API.Controllers;
 using System;
 using Xunit;
 
 namespace API.Tests
 {
-    public class UnitTest1
+    public class HelloFixture
     {
         [Fact]
-        public void Test1()
+        public void No_Parameter_Should_Return_BYE_Test()
         {
-
+            HelloController hello = new HelloController();
+            Assert.Equal("Bye", hello.Get().Value);
+        }
+        [Fact]
+        public void Hi_Parameter_Should_Return_Hello_Test()
+        {
+            HelloController hello = new HelloController();
+            Assert.Equal("Hello", hello.Get("Hi").Value);
+        }
+        [Fact]
+        public void Hello_Parameter_Should_Return_Hi_Test()
+        {
+            HelloController hello = new HelloController();
+            Assert.Equal("Hi", hello.Get("Hello").Value);
+        }
+        [Fact]
+        public void Default_Value_Test()
+        {
+            HelloController hello = new HelloController();
+            Assert.Equal("Say Hello or Hi First", hello.Get("abcd").Value);
         }
     }
 }
